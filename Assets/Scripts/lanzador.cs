11d10a5a9ca73f9ccdd04a12a6b57ec62a210dac
Brian Rodríguez <Brian.ERC@gmail.com>
2018-09-29 18:31:02 -0300

    [Add] Selector
    Ahora si agrego el selector que permite saber que posicion esta
    seleccionando para posicionar la estructura.

--- Assets/Scripts/lanzador.cs
+++ Assets/Scripts/lanzador.cs
@@ -5,13 +5,21 @@ public class Lanzador : MonoBehaviour {
     public GameObject estructura;
     private bool estado = false;
     public Grid matriz;
-
+    private GameObject planoPosicion;
+    private Color verde;
+    private Color rojo;
+    private Color transparente;
     // Use this for initialization
     void Start() {
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Lanzadores/" + estructura.name);
+        CargarSprite();
+        verde = new Color(0, 1, 0, 0.5f);
+        rojo = new Color(1, 0, 0, 0.5f);
+        transparente = new Color(0, 0, 0, 0);
+
     }
 
     // Update is called once per frame
+    
     void Update () {
         RaycastHit hit;
         if (Input.touchCount < 1)
@@ -27,6 +35,20 @@ public class Lanzador : MonoBehaviour {
                 if (hit.transform.tag == this.gameObject.tag)
                 {
                     estado = true;
+                    CrearSeleccion(touch);
+                    ActualizarPosicion(touch);
+                    ActualizarColorSeleccion(planoPosicion, hit);
+                }
+            }
+        }
+        if(touch.phase==TouchPhase.Moved && estado)
+        {
+            if(Physics.Raycast(ray,out hit))
+            {
+                if(hit.transform.tag == "Piso")
+                {
+                    GameObject.FindGameObjectWithTag("Seleccion").transform.position = ActualizarPosicion(touch);
+                    ActualizarColorSeleccion(GameObject.FindGameObjectWithTag("Seleccion"), hit);
                 }
             }
         }
@@ -41,19 +63,61 @@ public class Lanzador : MonoBehaviour {
                 }
                 else if (hit.transform.tag == "Piso")
                 {
-                    Vector3 punto = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
-                    Vector3Int auxiliar = matriz.WorldToCell(punto);
-                    punto = matriz.GetCellCenterWorld(auxiliar);
-                    estructura.transform.position = punto;
+                    estructura.transform.position = ActualizarPosicion(touch);
                     Instantiate(estructura);
                 }
                 else
                 {
-                    Debug.Log("No se puede colocar fuera de la matriz");
+                    Debug.Log("No se puede colocar fuera de la matriz: " + hit.transform.tag);
                 }
             }
+            Destroy(GameObject.FindGameObjectWithTag("Seleccion"));
+        }
+        if (touch.phase == TouchPhase.Canceled)
+        {
+            if(GameObject.FindGameObjectsWithTag("Seleccion")!=null)
+            {
+                Destroy(GameObject.FindGameObjectWithTag("Seleccion"));
+            }
+        }
+    }
+    private Vector3 ActualizarPosicion(Touch touch)
+    {
+        Vector3 punto = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
+        Vector3Int auxiliar = matriz.WorldToCell(punto);
+        punto = matriz.GetCellCenterWorld(auxiliar);
+        return punto;
+    }
+    private void CargarSprite()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Lanzadores/" + estructura.name);
+    }
+    private void CrearSeleccion(Touch touch)
+    {
+        planoPosicion = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        planoPosicion.transform.position = ActualizarPosicion(touch);
+        planoPosicion.tag = "Seleccion";
+        Destroy(planoPosicion.GetComponent<BoxCollider>());
+        planoPosicion.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materiales/Selector");
+        //planoPosicion.transform.localScale = new Vector3(1.2f, 1.2f, 0);
 
-
+        planoPosicion.GetComponent<MeshRenderer>().material.color = transparente;
+    }
+    private void ActualizarColorSeleccion(GameObject planoPosicion,RaycastHit hit)
+    {
+        MeshRenderer renderer = planoPosicion.GetComponent<MeshRenderer>();
+        renderer.material.color=transparente;
+        if (hit.transform.tag == estructura.tag)
+        {
+            renderer.material.color = rojo;
+        }
+        else if (hit.transform.tag == "Piso")
+        {
+            renderer.material.color = verde;
+        }
+        else
+        {
+            renderer.material.color = rojo;
         }
     }
 }
