2a53ab394ece7a7982f935248fc015f7b7ace018
Brian Rodriguez <brian.erc@gmail.com>
2018-10-03 14:06:55 -0300

    [Add] Contador

--- Assets/Scripts/comienzoPartida.cs
+++ Assets/Scripts/comienzoPartida.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ComienzoPartida : MonoBehaviour {
     public GameObject jugador;
     public GameObject jugador2;
     public Grid matriz;
-    public Arbol nerd;
+    public GameObject fondo;
+    public GameObject temporizador;
     public float separacionLanzadores;
+
 	// Use this for initialization
 	void Start () {
+
+        Instantiate(fondo);
         Instantiate(jugador);
         Instantiate(matriz);
         Instantiate(jugador2);
+        Instantiate(temporizador);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+
+        } 
 }
