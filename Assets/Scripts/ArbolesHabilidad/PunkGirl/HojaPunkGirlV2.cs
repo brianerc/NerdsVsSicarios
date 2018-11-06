f388f3e83148c291eeb64f807c9f3a2ecf665988
Brian Rodríguez <Brian.ERC@gmail.com>
2018-11-05 23:47:06 -0300

    [Add] PunkGirl Base - Falta Prefab del "player sicario punkgirl" (lanzador, etc)

---
+++ Assets/Scripts/ArbolesHabilidad/PunkGirl/HojaPunkGirlV2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HojaPunkGirlV2 : Hoja {
+
+    private PunkGirlV2 punkGirl;
+
+    public HojaPunkGirlV2(GameObject unObjeto)
+    {
+        nivel = 1;
+        nombre = "PunkGirl V2";
+        descripcion = "";
+        aprendida = true;
+        objeto = unObjeto;
+        punkGirl = objeto.GetComponent<PunkGirlV2>();
+        punkGirl.SetDaño(4);
+        punkGirl.SetVida(10);
+        punkGirl.SetVelocidad(-2f);
+        punkGirl.SetCostoEnergia(50);
+    }
+}
