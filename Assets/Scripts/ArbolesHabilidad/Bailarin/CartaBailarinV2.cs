66f6f7587bebe473994bf9eaa44069a13811915d
Brian Rodriguez <brian.erc@gmail.com>
2018-12-11 19:25:09 -0300

    [Add] Sicario 3 + presencia en SinglePlayer

---
+++ Assets/Scripts/ArbolesHabilidad/Bailarin/CartaBailarinV2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartaBailarinV2 : Carta
+{
+
+    private BailarinV2 bailarinV2;
+
+    public CartaBailarinV2(GameObject unObjeto)
+    {
+        nivel = 1;
+        nombre = "Bailarín Base";
+        descripcion = "";
+        aprendida = true;
+        objeto = unObjeto;
+        bailarinV2 = objeto.GetComponent<BailarinV2>();
+        bailarinV2.SetDaño(4);
+        bailarinV2.SetVida(10);
+        bailarinV2.SetVelocidad(-2f);
+        bailarinV2.SetCostoEnergia(50);
+        unObjeto.GetComponent<ObjetoTablero>().nivel = nivel;
+        unObjeto.GetComponent<ObjetoTablero>().nombre = nombre;
+    }
+}
