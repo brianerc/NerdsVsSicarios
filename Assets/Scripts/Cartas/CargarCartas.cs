92e1195023e239d4697b9da31fdf2cf2e13a134c
Brian Rodríguez <Brian.ERC@gmail.com>
2018-12-16 02:43:29 -0300

    [Add] Carrusel con scroll y desactivacion al pulsar las cartas

--- Assets/Scripts/Cartas/CargarCartas.cs
+++ Assets/Scripts/Cartas/CargarCartas.cs
@@ -9,24 +9,60 @@ public class CargarCartas : MonoBehaviour
 {
 
 	public RectTransform miPanel;
-	public GameObject spritePrefab;
+	public GameObject imagenPrefab;
 	private int miNumero = 0;
 	private GameObject textoNuevo;
 	public Text error;
-    public float xPosicion =0;
-    public float yPosicion = 0;
+    public float xPosicion =    50;
+    public float yPosicion =    50;
+    private string tagSeleccionado;
+    bool elegido;
     // Use this for initialization
     void Start()
 	{
 		error.text = "";
 		//nextMessage = Time.time + 1f;
 		StartCoroutine(ObtenerCartas());
-
+        elegido = false;
 	}
 	// Update is called once per frame
 	void Update()
 	{
-	}
+        if (Input.touchCount < 1)
+        {
+            return;
+        }
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Input.GetTouch(0).position);
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Moved)
+        {
+            if (hit.collider && tagSeleccionado != hit.collider.tag)
+            {
+                elegido = false;
+            } 
+        }
+        if (touch.phase == TouchPhase.Began)
+        {
+            if (hit.collider )
+            {
+                elegido = true;
+                tagSeleccionado = hit.collider.tag;
+            }
+            else
+            {
+                elegido = false;
+                tagSeleccionado = "";
+            }
+        }
+        if (touch.phase == TouchPhase.Ended)
+        {
+            if (hit.collider && hit.collider.tag.Contains("Lanzador" ) && elegido)
+            {
+                miPanel.gameObject.SetActive(false);
+            }
+        }
+    }
 
 	public IEnumerator ObtenerCartas()
 	{
@@ -62,10 +98,11 @@ public class CargarCartas : MonoBehaviour
 			for (int i = 0; i < resultObj.cartas.Count; i++)
 			{
 				Assets.Scripts.ServidorDTO.Carta carta = resultObj.cartas[i];
-				GameObject nuevoSprite = (GameObject)Instantiate(spritePrefab);
+				GameObject nuevoSprite = (GameObject)Instantiate(imagenPrefab);
                 nuevoSprite.transform.SetParent(miPanel);
-                nuevoSprite.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Partida/Lanzadores/" + carta.ToString());
+                nuevoSprite.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Partida/Lanzadores/" + carta.ToString());
                 nuevoSprite.transform.localPosition = new Vector2(xPosicion, yPosicion);
+                nuevoSprite.transform.tag = "Lanzador" + carta.nombre_completo;
                 xPosicion = xPosicion + 110;
                 miNumero++;
 			}
